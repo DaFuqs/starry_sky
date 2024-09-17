6dda45da1e4668c59997bc6bd97b2d07460a449f
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-09-17 17:59:05 +0200

    1.21.1

--- src/main/java/de/dafuqs/starryskies/mixin/EntityAccessor.java
+++ src/main/java/de/dafuqs/starryskies/mixin/EntityAccessor.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.gen.*;
 
 @Mixin(Entity.class)
-
 public interface EntityAccessor {
 
 	@Accessor("lastNetherPortalPosition")
