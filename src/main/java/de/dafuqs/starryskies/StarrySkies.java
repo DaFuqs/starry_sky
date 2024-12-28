4391eb35ea9ec4ebd74027a18a733e0b4eb176ee
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-12-28 13:32:45 +0100

    custom sleep logic. Fixes #13

--- src/main/java/de/dafuqs/starryskies/StarrySkies.java
+++ src/main/java/de/dafuqs/starryskies/StarrySkies.java
@@ -12,6 +12,7 @@ import me.shedaniel.autoconfig.*;
 import me.shedaniel.autoconfig.serializer.*;
 import net.fabricmc.api.*;
 import net.fabricmc.fabric.api.command.v2.*;
+import net.fabricmc.fabric.api.entity.event.v1.*;
 import net.fabricmc.fabric.api.event.lifecycle.v1.*;
 import net.kyrptonaught.customportalapi.*;
 import net.kyrptonaught.customportalapi.util.*;
@@ -19,7 +20,9 @@ import net.minecraft.block.*;
 import net.minecraft.command.argument.*;
 import net.minecraft.registry.*;
 import net.minecraft.server.network.*;
+import net.minecraft.server.world.*;
 import net.minecraft.util.*;
+import net.minecraft.world.*;
 import net.minecraft.world.gen.chunk.*;
 import org.jetbrains.annotations.*;
 import org.slf4j.*;
@@ -27,23 +30,23 @@ import org.slf4j.*;
 import java.util.*;
 
 public class StarrySkies implements ModInitializer {
-
+	
 	public static final String MOD_ID = "starry_skies";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static StarrySkyConfig CONFIG;
-
+	
 	public static Identifier id(String name) {
 		return Identifier.of(MOD_ID, name);
 	}
-
+	
 	public static String idPlain(String name) {
 		return id(name).toString();
 	}
-
+	
 	public static BlockState getStateFromString(String s) throws CommandSyntaxException {
 		return BlockArgumentParser.block(Registries.BLOCK.getReadOnlyWrapper(), s, false).blockState();
 	}
-
+	
 	public static @Nullable BlockState getNullableStateFromString(String s) {
 		try {
 			return BlockArgumentParser.block(Registries.BLOCK.getReadOnlyWrapper(), s, false).blockState();
@@ -52,7 +55,7 @@ public class StarrySkies implements ModInitializer {
 			return null;
 		}
 	}
-
+	
 	public static @Nullable BlockArgumentParser.BlockResult getBlockResult(String element) {
 		try {
 			return BlockArgumentParser.block(Registries.BLOCK.getReadOnlyWrapper(), element, true);
@@ -61,19 +64,19 @@ public class StarrySkies implements ModInitializer {
 			return null;
 		}
 	}
-
-	public static boolean inStarryWorld(ServerPlayerEntity serverPlayerEntity) {
-		ChunkGenerator chunkGenerator = serverPlayerEntity.getServerWorld().getChunkManager().getChunkGenerator();
+	
+	public static boolean isStarryWorld(ServerWorld world) {
+		ChunkGenerator chunkGenerator = world.getChunkManager().getChunkGenerator();
 		return chunkGenerator instanceof StarrySkyChunkGenerator;
 	}
-
+	
 	@Override
 	public void onInitialize() {
 		//Set up config
 		LOGGER.info("Starting up...");
 		AutoConfig.register(StarrySkyConfig.class, JanksonConfigSerializer::new);
 		CONFIG = AutoConfig.getConfigHolder(StarrySkyConfig.class).getConfig();
-
+		
 		// Register all the stuff
 		Registry.register(Registries.CHUNK_GENERATOR, StarrySkies.id("starry_skies"), StarrySkyChunkGenerator.CODEC);
 		
@@ -90,7 +93,7 @@ public class StarrySkies implements ModInitializer {
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
 			Set<Map.Entry<RegistryKey<ConfiguredSphere<?, ?>>, ConfiguredSphere<?, ?>>> allSpheres = server.getRegistryManager().get(StarryRegistryKeys.CONFIGURED_SPHERE).getEntrySet();
 			
-			for(var generationGroup : server.getRegistryManager().get(StarryRegistryKeys.GENERATION_GROUP).getEntrySet()) {
+			for (var generationGroup : server.getRegistryManager().get(StarryRegistryKeys.GENERATION_GROUP).getEntrySet()) {
 				Identifier systemGeneratorId = generationGroup.getValue().systemGeneratorId();
 				
 				SystemGenerator systemGenerator = server.getRegistryManager().get(StarryRegistryKeys.SYSTEM_GENERATOR).get(systemGeneratorId);
@@ -113,21 +116,42 @@ public class StarrySkies implements ModInitializer {
 			}
 		});
 		
+		/*
+			Workaround for
+			https://bugs.mojang.com/browse/MC-188578: Sleeping in a bed in a custom dimension doesn't set time to day
+			Weather and time of day is also only tracked in the overworld
+		 */
+		EntitySleepEvents.STOP_SLEEPING.register((entity, sleepingPos) -> {
+			if (entity instanceof ServerPlayerEntity serverPlayerEntity) {
+				ServerWorld world = serverPlayerEntity.getServerWorld();
+				if (isStarryWorld(world) && serverPlayerEntity.canResetTimeBySleeping()) {
+					long nextDay = world.getTimeOfDay() + 24000L;
+					long mod = nextDay - nextDay % 24000L;
+					world.getServer().getOverworld().setTimeOfDay(mod);
+					
+					if (world.getGameRules().getBoolean(GameRules.DO_WEATHER_CYCLE) && world.isRaining()) {
+						world.getServer().getOverworld().resetWeather();
+					}
+				}
+			}
+		});
+		
+		
 		if (CONFIG.registerStarryPortal) {
 			setupPortals();
 		}
-
+		
 		LOGGER.info("Finished loading.");
 	}
-
+	
 	public static void setupPortals() {
 		StarrySkies.LOGGER.info("Setting up Portal to Starry Skies...");
 		
 		Identifier portalFrameBlockIdentifier = Identifier.tryParse(StarrySkies.CONFIG.starrySkyPortalFrameBlock.toLowerCase());
 		Block portalFrameBlock = Registries.BLOCK.get(portalFrameBlockIdentifier);
-
+		
 		PortalLink portalLink = new PortalLink(portalFrameBlockIdentifier, StarryDimensionKeys.STARRY_SKIES_DIMENSION_ID, StarrySkies.CONFIG.starrySkyPortalColor);
 		CustomPortalApiRegistry.addPortal(portalFrameBlock, portalLink);
 	}
-
+	
 }
