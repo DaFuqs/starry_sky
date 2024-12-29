12107a477ff0e92b0e4bd3b435dbebb1b726699d
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-12-29 11:28:11 +0100

    fluid spheres

--- src/main/java/de/dafuqs/starryskies/worldgen/Spheres.java
+++ src/main/java/de/dafuqs/starryskies/worldgen/Spheres.java
@@ -13,6 +13,7 @@ public class Spheres {
 	public static final Sphere<ModularSphere.Config> MODULAR = register("modular", new ModularSphere(ModularSphere.Config.CODEC));
 	public static final Sphere<CaveSphere.Config> CAVE = register("cave", new CaveSphere(CaveSphere.Config.CODEC));
 	public static final Sphere<DungeonSphere.Config> DUNGEON = register("dungeon", new DungeonSphere(DungeonSphere.Config.CODEC));
+	public static final Sphere<FluidSphere.Config> FLUID = register("fluid", new FluidSphere(FluidSphere.Config.CODEC));
 	
 	/*
 	public static final Sphere<NetherFortressSphere.Config> NETHER_FORTRESS = register("nether_fortress", NetherFortressSphere.Config.CODEC);
@@ -21,7 +22,6 @@ public class Spheres {
 	public static final Sphere<StrongholdSphere.Config> STRONGHOLD = register("stronghold", StrongholdSphere.Config.CODEC);
 	public static final Sphere<StackedHorizontalSphere.Config> STACKED_HORIZONTAL = register("stacked_horizontal", StackedHorizontalSphere.Config.CODEC);
 	public static final Sphere<EndCitySphere.Config> END_CITY = register("end_city", EndCitySphere.Config.CODEC);
-	public static final Sphere<FluidSphere.Config> FLUID = register("fluid", FluidSphere.Config.CODEC);
 	public static final Sphere<FluidCoreSphere.Config> CORE_FLUID = register("fluid_core", FluidCoreSphere.Config.CODEC);
 	public static final Sphere<MushroomSphere.Config> MUSHROOM = register("mushroom", MushroomSphere.Config.CODEC);
 	public static final Sphere<ShellCoreSphere.Config> SHELL_CORE = register("shell_core", ShellCoreSphere.Config.CODEC);
---
+++ src/main/java/de/dafuqs/starryskies/worldgen/spheres/FluidSphere.java
@@ -0,0 +1,137 @@
+package de.dafuqs.starryskies.worldgen.spheres;
+
+import com.mojang.serialization.*;
+import com.mojang.serialization.codecs.*;
+import de.dafuqs.starryskies.*;
+import de.dafuqs.starryskies.worldgen.*;
+import net.minecraft.block.*;
+import net.minecraft.entity.*;
+import net.minecraft.registry.*;
+import net.minecraft.util.*;
+import net.minecraft.util.dynamic.*;
+import net.minecraft.util.math.*;
+import net.minecraft.util.math.floatprovider.*;
+import net.minecraft.util.math.intprovider.*;
+import net.minecraft.util.math.random.*;
+import net.minecraft.world.chunk.*;
+import net.minecraft.world.gen.stateprovider.*;
+
+import java.util.*;
+
+public class FluidSphere extends Sphere<FluidSphere.Config> {
+	
+	public FluidSphere(Codec<FluidSphere.Config> codec) {
+		super(codec);
+	}
+	
+	@Override
+	public PlacedSphere<?> generate(ConfiguredSphere<? extends Sphere<FluidSphere.Config>, Config> configuredSphere, Config config, ChunkRandom random, DynamicRegistryManager registryManager) {
+		return new FluidSphere.Placed(configuredSphere, configuredSphere.getSize(random), configuredSphere.getDecorators(random), configuredSphere.getSpawns(random), random, config.shellBlock, config.shellThickness.get(random), config.fluidBlock, config.fillPercent.get(random), config.holeInBottomChance > random.nextFloat());
+	}
+	
+	public static class Config extends SphereConfig {
+		
+		public static final Codec<FluidSphere.Config> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+				SphereConfig.CONFIG_CODEC.forGetter((config) -> config),
+				BlockStateProvider.TYPE_CODEC.fieldOf("shell_block").forGetter((config) -> config.shellBlock),
+				IntProvider.POSITIVE_CODEC.fieldOf("shell_thickness").forGetter((config) -> config.shellThickness),
+				BlockState.CODEC.fieldOf("fluid_block").forGetter((config) -> config.fluidBlock),
+				FloatProvider.createValidatedCodec(0.0F, 1.0F).fieldOf("fluid_fill_percent").forGetter((config) -> config.fillPercent),
+				Codecs.POSITIVE_FLOAT.fieldOf("hole_in_bottom_chance").forGetter((config) -> config.holeInBottomChance)
+		).apply(instance, (sphereConfig, shellBlock, shellThickness, fluidBlock, fillAmount, holeInBottom) -> new Config(sphereConfig.size, sphereConfig.decorators, sphereConfig.spawns, sphereConfig.generation, shellBlock, shellThickness, fluidBlock, fillAmount, holeInBottom)));
+		
+		protected final BlockStateProvider shellBlock;
+		protected final IntProvider shellThickness;
+		protected final BlockState fluidBlock;
+		protected final FloatProvider fillPercent;
+		protected final float holeInBottomChance;
+		
+		public Config(FloatProvider size, Map<ConfiguredSphereDecorator<?, ?>, Float> decorators, List<SphereEntitySpawnDefinition> spawns, Optional<Generation> generation, BlockStateProvider shellBlock, IntProvider shellThickness, BlockState fluidBlock, FloatProvider fillPercent, float holeInBottomChance) {
+			super(size, decorators, spawns, generation);
+			this.shellBlock = shellBlock;
+			this.shellThickness = shellThickness;
+			this.fluidBlock = fluidBlock;
+			this.fillPercent = fillPercent;
+			this.holeInBottomChance = holeInBottomChance;
+		}
+		
+	}
+	
+	public static class Placed extends PlacedSphere<FluidSphere.Config> {
+		
+		private final BlockState CAVE_AIR = Blocks.CAVE_AIR.getDefaultState();
+		
+		private final BlockStateProvider shellBlock;
+		private final float shellRadius;
+		private final BlockState fluidBlock;
+		private final float fillPercent;
+		private final boolean holeInBottom;
+		
+		public Placed(ConfiguredSphere<? extends Sphere<FluidSphere.Config>, FluidSphere.Config> configuredSphere, float radius, List<ConfiguredSphereDecorator<?, ?>> decorators, List<Pair<EntityType<?>, Integer>> spawns, ChunkRandom random,
+					  BlockStateProvider shellBlock, float shellRadius, BlockState fluidBlock, float fillPercent, boolean holeInBottom) {
+			super(configuredSphere, radius, decorators, spawns, random);
+			this.shellBlock = shellBlock;
+			this.shellRadius = shellRadius;
+			this.fluidBlock = fluidBlock;
+			this.fillPercent = fillPercent;
+			this.holeInBottom = holeInBottom;
+		}
+		
+		@Override
+		public void generate(Chunk chunk, DynamicRegistryManager registryManager) {
+			int chunkX = chunk.getPos().x;
+			int chunkZ = chunk.getPos().z;
+			random.setSeed(chunkX * 341873128712L + chunkZ * 132897987541L);
+			int x = this.getPosition().getX();
+			int y = this.getPosition().getY();
+			int z = this.getPosition().getZ();
+			
+			int ceiledRadius = (int) Math.ceil(this.radius);
+			int maxX = Math.min(chunkX * 16 + 15, x + ceiledRadius);
+			int maxZ = Math.min(chunkZ * 16 + 15, z + ceiledRadius);
+			
+			float liquidRadius = this.radius - this.shellRadius;
+			float maxLiquidY = y + (this.fillPercent * liquidRadius * 2 - liquidRadius);
+			
+			BlockPos.Mutable currBlockPos = new BlockPos.Mutable();
+			for (int x2 = Math.max(chunkX * 16, x - ceiledRadius); x2 <= maxX; x2++) {
+				for (int y2 = y - ceiledRadius; y2 <= y + ceiledRadius; y2++) {
+					for (int z2 = Math.max(chunkZ * 16, z - ceiledRadius); z2 <= maxZ; z2++) {
+						long d = Math.round(Support.getDistance(x, y, z, x2, y2, z2));
+						if (d > this.radius) {
+							continue;
+						}
+						currBlockPos.set(x2, y2, z2);
+						
+						if (this.holeInBottom && (x - x2) == 0 && (z - z2) == 0 && (y - y2 + 1) >= liquidRadius) {
+							chunk.setBlockState(new BlockPos(currBlockPos), this.fluidBlock, false);
+							chunk.markBlockForPostProcessing(currBlockPos); // making it drip down after generation
+						} else if (d <= liquidRadius) {
+							if (y2 <= maxLiquidY) {
+								chunk.setBlockState(currBlockPos, this.fluidBlock, false);
+							} else {
+								chunk.setBlockState(currBlockPos, CAVE_AIR, false);
+							}
+						} else {
+							chunk.setBlockState(currBlockPos, this.shellBlock.get(random, currBlockPos), false);
+						}
+					}
+				}
+			}
+		}
+		
+		@Override
+		public String getDescription(DynamicRegistryManager registryManager) {
+			return "+++ FluidSphere +++" +
+					"\nPosition: x=" + this.getPosition().getX() + " y=" + this.getPosition().getY() + " z=" + this.getPosition().getZ() +
+					"\nTemplateID: " + this.getID(registryManager) +
+					"\nRadius: " + this.radius +
+					"\nShell: " + this.shellBlock.toString() + "(Radius: " + this.shellRadius + ")" +
+					"\nLiquid: " + this.fluidBlock.toString() +
+					"\nFill Percent: " + this.fillPercent +
+					"\nHole in bottom: " + this.holeInBottom;
+		}
+	}
+	
+}
+	
