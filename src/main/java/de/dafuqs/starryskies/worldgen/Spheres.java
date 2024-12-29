9682e6679344a44bb72dd3473da9dc7795a81285
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-12-29 11:48:32 +0100

    fluid core spheres

--- src/main/java/de/dafuqs/starryskies/worldgen/Spheres.java
+++ src/main/java/de/dafuqs/starryskies/worldgen/Spheres.java
@@ -14,6 +14,7 @@ public class Spheres {
 	public static final Sphere<CaveSphere.Config> CAVE = register("cave", new CaveSphere(CaveSphere.Config.CODEC));
 	public static final Sphere<DungeonSphere.Config> DUNGEON = register("dungeon", new DungeonSphere(DungeonSphere.Config.CODEC));
 	public static final Sphere<FluidSphere.Config> FLUID = register("fluid", new FluidSphere(FluidSphere.Config.CODEC));
+	public static final Sphere<FluidCoreSphere.Config> FLUID_CORE = register("fluid_core", new FluidCoreSphere(FluidCoreSphere.Config.CODEC));
 	
 	/*
 	public static final Sphere<NetherFortressSphere.Config> NETHER_FORTRESS = register("nether_fortress", NetherFortressSphere.Config.CODEC);
@@ -22,7 +23,6 @@ public class Spheres {
 	public static final Sphere<StrongholdSphere.Config> STRONGHOLD = register("stronghold", StrongholdSphere.Config.CODEC);
 	public static final Sphere<StackedHorizontalSphere.Config> STACKED_HORIZONTAL = register("stacked_horizontal", StackedHorizontalSphere.Config.CODEC);
 	public static final Sphere<EndCitySphere.Config> END_CITY = register("end_city", EndCitySphere.Config.CODEC);
-	public static final Sphere<FluidCoreSphere.Config> CORE_FLUID = register("fluid_core", FluidCoreSphere.Config.CODEC);
 	public static final Sphere<MushroomSphere.Config> MUSHROOM = register("mushroom", MushroomSphere.Config.CODEC);
 	public static final Sphere<ShellCoreSphere.Config> SHELL_CORE = register("shell_core", ShellCoreSphere.Config.CODEC);
 	public static final Sphere<BeeHiveSphere.Config> BEE_HIVE = register("bee_hive", BeeHiveSphere.Config.CODEC);
