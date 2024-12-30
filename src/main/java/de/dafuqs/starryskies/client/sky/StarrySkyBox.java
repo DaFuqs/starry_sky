bbeb4d1b1695a221b71444ef171f699280255bb9
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-12-30 18:58:50 +0100

    skybox working!

--- src/main/java/de/dafuqs/starryskies/client/sky/StarrySkyBox.java
+++ src/main/java/de/dafuqs/starryskies/client/sky/StarrySkyBox.java
@@ -4,10 +4,11 @@ import com.mojang.blaze3d.systems.*;
 import de.dafuqs.starryskies.*;
 import net.fabricmc.api.*;
 import net.fabricmc.fabric.api.client.rendering.v1.*;
+import net.minecraft.block.enums.*;
 import net.minecraft.client.render.*;
-import net.minecraft.client.util.math.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.effect.*;
 import net.minecraft.util.*;
-import net.minecraft.util.math.*;
 import org.joml.*;
 
 // TODO: Rainbow Skybox setting does not work
@@ -32,62 +33,90 @@ public class StarrySkyBox implements DimensionRenderingRegistry.SkyRenderer {
 
 	@Override
 	public void render(WorldRenderContext context) {
-		MatrixStack matrixStack = new MatrixStack();
-		matrixStack.multiplyPositionMatrix(context.projectionMatrix());
-		renderStarrySky(matrixStack);
+		CameraSubmersionType cameraSubmersionType = context.camera().getSubmersionType();
+		if (cameraSubmersionType == CameraSubmersionType.POWDER_SNOW || cameraSubmersionType == CameraSubmersionType.LAVA || hasBlindnessOrDarkness(context.camera())) {
+			return;
+		}
+		renderStarrySky(context);
+	}
+	
+	private boolean hasBlindnessOrDarkness(Camera camera) {
+		Entity var3 = camera.getFocusedEntity();
+		if (!(var3 instanceof LivingEntity livingEntity)) {
+			return false;
+		} else {
+			return livingEntity.hasStatusEffect(StatusEffects.BLINDNESS) || livingEntity.hasStatusEffect(StatusEffects.DARKNESS);
+		}
 	}
 	
 	// See WorldRenderer.renderEndSky() for inspiration
-	private void renderStarrySky(MatrixStack matrices) {
+	private void renderStarrySky(WorldRenderContext context) {
 		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
 		RenderSystem.depthMask(false);
 		RenderSystem.setShader(GameRenderer::getPositionTexColorProgram);
 		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
 		
-		for (int i = 0; i < 6; ++i) {
-			matrices.push();
-			
-			if (i == 0) {
-				RenderSystem.setShaderTexture(0, DOWN);
-			}
-			
-			if (i == 1) {
-				RenderSystem.setShaderTexture(0, WEST);
-				matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90.0F));
-			}
-			
-			if (i == 2) {
-				RenderSystem.setShaderTexture(0, EAST);
-				matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-90.0F));
-			}
-			
-			if (i == 3) {
-				RenderSystem.setShaderTexture(0, UP);
-				matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180.0F));
-			}
-			
-			if (i == 4) {
-				RenderSystem.setShaderTexture(0, NORTH);
-				matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(90.0F));
-			}
-			
-			if (i == 5) {
-				RenderSystem.setShaderTexture(0, SOUTH);
-				matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(-90.0F));
-			}
-			
-			Matrix4f matrix4f = matrices.peek().getPositionMatrix();
-			BufferBuilder bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
-			bufferBuilder.vertex(matrix4f, -100.0F, -100.0F, -100.0F).texture(0.0F, 0.0F).color(-14145496);
-			bufferBuilder.vertex(matrix4f, -100.0F, -100.0F, 100.0F).texture(0.0F, 16.0F).color(-14145496);
-			bufferBuilder.vertex(matrix4f, 100.0F, -100.0F, 100.0F).texture(16.0F, 16.0F).color(-14145496);
-			bufferBuilder.vertex(matrix4f, 100.0F, -100.0F, -100.0F).texture(16.0F, 0.0F).color(-14145496);
-			BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
-			matrices.pop();
-		}
+		Matrix4f matrix4f = context.positionMatrix();
+		
+		/*Vec3d vec3d = context.world().getSkyColor(context.camera().getPos(), 0);  // TODO: how to get tickdelta?
+		float f = (float)vec3d.x;
+		float g = (float)vec3d.y;
+		float h = (float)vec3d.z;
+		RenderSystem.setShaderColor(f, g, h, 1.0F);*/
+		
+		RenderSystem.setShaderTexture(0, DOWN);
+		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, -100.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, 100.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, 100.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, -100.0f).texture(1.0F, 0.0F).color(255, 255, 255, 255);
+		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+		
+		RenderSystem.setShaderTexture(0, WEST);
+		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		bufferBuilder.vertex(matrix4f, -100.0f, 100.0f, -100.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, -99.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, -99.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, 100.0f, -100.0f).texture(1.0F, 0.0F).color(255, 255, 255, 255);
+		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+		
+		RenderSystem.setShaderTexture(0, EAST);
+		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, 100.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, 100.0f, 100.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, 100.0f, 100.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, 100.0f).texture(1.0F, 0.0F).color(255, 255, 255, 255);
+		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+		
+		RenderSystem.setShaderTexture(0, UP);
+		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		bufferBuilder.vertex(matrix4f, -100.0f, 100.0f, 101.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, 100.0f, -100.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, 100.0f, -100.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, 100.0f, 100.0f).texture(1.0F, 0.0F).color(255, 255, 255, 255);
+		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+		
+		RenderSystem.setShaderTexture(0, NORTH);
+		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, -100.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, -100.0f, 100.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, 100.0f, 100.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, 100.0f, 100.0f, -100.0f).texture(1.0F, 0.0F).color(255, 255, 255, 255);
+		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+		
+		RenderSystem.setShaderTexture(0, SOUTH);
+		bufferBuilder = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
+		bufferBuilder.vertex(matrix4f, -100.0f, 100.0f, -100.0f).texture(0.0F, 0.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, 100.0f, 100.0f).texture(0.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, 100.0f).texture(1.0F, 1.0F).color(255, 255, 255, 255);
+		bufferBuilder.vertex(matrix4f, -100.0f, -100.0f, -100.0f).texture(1.0F, 0.0F).color(255, 255, 255, 255);
+		BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
 		
 		RenderSystem.depthMask(true);
 		RenderSystem.disableBlend();
 	}
-
+	
+	
 }
