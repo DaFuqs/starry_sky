1cd466e898adfeddbd01d018e811f789ad74bcea
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-12-31 13:10:36 +0100

    made geode spheres more modular. Now grows clusters

--- src/main/java/de/dafuqs/starryskies/worldgen/spheres/GeodeSphere.java
+++ src/main/java/de/dafuqs/starryskies/worldgen/spheres/GeodeSphere.java
@@ -3,17 +3,20 @@ package de.dafuqs.starryskies.worldgen.spheres;
 import com.mojang.serialization.*;
 import com.mojang.serialization.codecs.*;
 import de.dafuqs.starryskies.*;
+import de.dafuqs.starryskies.state_providers.*;
 import de.dafuqs.starryskies.worldgen.*;
 import net.minecraft.block.*;
 import net.minecraft.entity.*;
 import net.minecraft.registry.*;
 import net.minecraft.registry.entry.*;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.*;
 import net.minecraft.util.dynamic.*;
 import net.minecraft.util.math.*;
 import net.minecraft.util.math.floatprovider.*;
 import net.minecraft.util.math.random.*;
 import net.minecraft.world.chunk.*;
+import net.minecraft.world.gen.stateprovider.*;
 
 import java.util.*;
 
@@ -25,32 +28,37 @@ public class GeodeSphere extends Sphere<GeodeSphere.Config> {
 	
 	@Override
 	public PlacedSphere<?> generate(ConfiguredSphere<? extends Sphere<GeodeSphere.Config>, Config> configuredSphere, Config config, ChunkRandom random, DynamicRegistryManager registryManager, BlockPos pos, float radius) {
-		return new GeodeSphere.Placed(configuredSphere, radius, configuredSphere.getDecorators(random), configuredSphere.getSpawns(random), random, config.innerBlockState, config.innerSpecklesBlockState, config.speckleChance, config.middleBlockState, config.outerBlockState);
+		return new GeodeSphere.Placed(configuredSphere, radius, configuredSphere.getDecorators(random), configuredSphere.getSpawns(random), random,
+				config.innerBlockState.getForSphere(random, pos), config.innerSpecklesBlockState.getForSphere(random, pos), config.speckleChance, config.innerSpecklesAttachedBlockState.getForSphere(random, pos), config.middleBlockState.getForSphere(random, pos), config.outerBlockState.getForSphere(random, pos));
 	}
 	
 	public static class Config extends SphereConfig {
 		
 		public static final Codec<GeodeSphere.Config> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
 				SphereConfig.CONFIG_CODEC.forGetter((config) -> config),
-				BlockState.CODEC.fieldOf("inner_block").forGetter((config) -> config.innerBlockState),
-				BlockState.CODEC.fieldOf("inner_speckles_block").forGetter((config) -> config.innerSpecklesBlockState),
+				SphereStateProvider.CODEC.fieldOf("inner_block").forGetter((config) -> config.innerBlockState),
+				SphereStateProvider.CODEC.fieldOf("inner_speckles_block").forGetter((config) -> config.innerSpecklesBlockState),
 				Codecs.POSITIVE_FLOAT.fieldOf("inner_speckles_block_chance").forGetter((config) -> config.speckleChance),
-				BlockState.CODEC.fieldOf("middle_block").forGetter((config) -> config.middleBlockState),
-				BlockState.CODEC.fieldOf("outer_block").forGetter((config) -> config.outerBlockState)
-		).apply(instance, (sphereConfig, innerBlockState, innerSpecklesBlockState, speckleChance, middleBlockState, outerBlockState) -> new Config(sphereConfig.size, sphereConfig.decorators, sphereConfig.spawns, sphereConfig.generation, innerBlockState, innerSpecklesBlockState, speckleChance, middleBlockState, outerBlockState)));
+				SphereStateProvider.CODEC.fieldOf("inner_speckles_attached_block").forGetter((config) -> config.innerSpecklesAttachedBlockState),
+				SphereStateProvider.CODEC.fieldOf("middle_block").forGetter((config) -> config.middleBlockState),
+				SphereStateProvider.CODEC.fieldOf("outer_block").forGetter((config) -> config.outerBlockState)
+		).apply(instance, (sphereConfig, innerBlockState, innerSpecklesBlockState, speckleChance, innerSpecklesAttachedBlockState, middleBlockState, outerBlockState)
+				-> new Config(sphereConfig.size, sphereConfig.decorators, sphereConfig.spawns, sphereConfig.generation, innerBlockState, innerSpecklesBlockState, speckleChance, innerSpecklesAttachedBlockState, middleBlockState, outerBlockState)));
 		
-		private final BlockState innerBlockState;
-		private final BlockState innerSpecklesBlockState;
+		private final SphereStateProvider innerBlockState;
+		private final SphereStateProvider innerSpecklesBlockState;
 		private final float speckleChance;
-		private final BlockState middleBlockState;
-		private final BlockState outerBlockState;
+		private final SphereStateProvider innerSpecklesAttachedBlockState;
+		private final SphereStateProvider middleBlockState;
+		private final SphereStateProvider outerBlockState;
 		
 		public Config(FloatProvider size, Map<RegistryEntry<ConfiguredSphereDecorator<?, ?>>, Float> decorators, List<SphereEntitySpawnDefinition> spawns, Optional<Generation> generation,
-					  BlockState innerBlockState, BlockState innerSpecklesBlockState, float speckleChance, BlockState middleBlockState, BlockState outerBlockState) {
+					  SphereStateProvider innerBlockState, SphereStateProvider innerSpecklesBlockState, float speckleChance, SphereStateProvider innerSpecklesAttachedBlockState, SphereStateProvider middleBlockState, SphereStateProvider outerBlockState) {
 			super(size, decorators, spawns, generation);
 			this.innerBlockState = innerBlockState;
 			this.innerSpecklesBlockState = innerSpecklesBlockState;
 			this.speckleChance = speckleChance;
+			this.innerSpecklesAttachedBlockState = innerSpecklesAttachedBlockState;
 			this.middleBlockState = middleBlockState;
 			this.outerBlockState = outerBlockState;
 		}
@@ -59,18 +67,20 @@ public class GeodeSphere extends Sphere<GeodeSphere.Config> {
 	
 	public static class Placed extends PlacedSphere<GeodeSphere.Config> {
 		
-		private final BlockState innerBlockState;
-		private final BlockState innerSpecklesBlockState;
+		private final BlockStateProvider innerBlockState;
+		private final BlockStateProvider innerSpecklesBlockState;
 		private final float speckleChance;
-		private final BlockState middleBlockState;
-		private final BlockState outerBlockState;
+		private final BlockStateProvider innerSpecklesAttachedBlockState;
+		private final BlockStateProvider middleBlockState;
+		private final BlockStateProvider outerBlockState;
 		
 		public Placed(ConfiguredSphere<? extends Sphere<GeodeSphere.Config>, GeodeSphere.Config> configuredSphere, float radius, List<RegistryEntry<ConfiguredSphereDecorator<?, ?>>> decorators, List<Pair<EntityType<?>, Integer>> spawns, ChunkRandom random,
-					  BlockState innerBlockState, BlockState innerSpecklesBlockState, float speckleChance, BlockState middleBlockState, BlockState outerBlockState) {
+					  BlockStateProvider innerBlockState, BlockStateProvider innerSpecklesBlockState, float speckleChance, BlockStateProvider innerSpecklesAttachedBlockState, BlockStateProvider middleBlockState, BlockStateProvider outerBlockState) {
 			super(configuredSphere, radius, decorators, spawns, random);
 			this.innerBlockState = innerBlockState;
 			this.innerSpecklesBlockState = innerSpecklesBlockState;
 			this.speckleChance = speckleChance;
+			this.innerSpecklesAttachedBlockState = innerSpecklesAttachedBlockState;
 			this.middleBlockState = middleBlockState;
 			this.outerBlockState = outerBlockState;
 		}
@@ -103,14 +113,29 @@ public class GeodeSphere extends Sphere<GeodeSphere.Config> {
 							// nothing
 						} else if (d < this.radius - 3) {
 							if (random.nextFloat() < speckleChance) {
-								chunk.setBlockState(currBlockPos, innerSpecklesBlockState, false);
+								chunk.setBlockState(currBlockPos, innerSpecklesBlockState.get(random, currBlockPos), false);
+								
+								// since we are operating on a chunk-by-chunk basis,
+								// we cannot spill into neighnoring chunks. So if a budding block
+								// is at a chunk border it will not have attached crystals. Big sad
+								for (Direction direction : Direction.values()) {
+									BlockPos posInDirection = currBlockPos.offset(direction);
+									BlockState crystalState = innerSpecklesAttachedBlockState.get(random, posInDirection);
+									if (Support.isBlockPosInChunkPos(chunk.getPos(), posInDirection) && BuddingAmethystBlock.canGrowIn(chunk.getBlockState(posInDirection))) {
+										if (crystalState.contains(Properties.FACING)) {
+											crystalState = crystalState.with(Properties.FACING, direction);
+										}
+										chunk.setBlockState(posInDirection, crystalState, false);
+									}
+								}
+								
 							} else {
-								chunk.setBlockState(currBlockPos, innerBlockState, false);
+								chunk.setBlockState(currBlockPos, innerBlockState.get(random, currBlockPos), false);
 							}
 						} else if (d < this.radius - 2) {
-							chunk.setBlockState(currBlockPos, middleBlockState, false);
+							chunk.setBlockState(currBlockPos, middleBlockState.get(random, currBlockPos), false);
 						} else if (d < this.radius - 1) {
-							chunk.setBlockState(currBlockPos, outerBlockState, false);
+							chunk.setBlockState(currBlockPos, outerBlockState.get(random, currBlockPos), false);
 						}
 					}
 				}
